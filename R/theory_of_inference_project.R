6ba563e68d6e942cb8feebe5e48052ac24c553ee
nathann0209 <nathan980.sarpong@gmail.com>
2025-03-18 00:03:54 +0000

    adding residual analysis 3(iv)

--- R/theory_of_inference_project.R
+++ R/theory_of_inference_project.R
@@ -17,8 +17,8 @@ neg_log_likelihood <- function(params, likelihood_func, times, init, fin) {
   return(-log_likelihood)
 }
 
-# Marie's model likelihood function
-marie_likelihood <- function(params, times, init, fin) {
+# model_3's model likelihood function
+model_3_likelihood <- function(params, times, init, fin) {
   # Extract parameters
   theta1 <- params[1]
   theta2 <- params[2]
@@ -35,14 +35,14 @@ marie_likelihood <- function(params, times, init, fin) {
   # Standard deviation scales with initial count
   sd_fin <- init * sqrt(s2)
   
-  # Calculate likelihood using the correct error structure
+  # Calculate likelihood
   likelihoods <- dnorm(fin, mean = expected_fin, sd = sd_fin)
   
   return(likelihoods)
 }
 
-# Function to fit Marie's model using maximum likelihood
-fit_marie_model <- function(surv) {
+# Function to fit model_3's model using maximum likelihood
+fit_model_3_model <- function(surv) {
   # Extract data from surv
   times <- surv$times
   init <- surv$init
@@ -69,7 +69,7 @@ fit_marie_model <- function(surv) {
   optim_result <- optim(
     par = initial_params,
     fn = neg_log_likelihood,
-    likelihood_func = marie_likelihood,
+    likelihood_func = model_3_likelihood,
     times = times,
     init = init,
     fin = fin,
@@ -80,19 +80,73 @@ fit_marie_model <- function(surv) {
 
   # Check convergence
   if (optim_result$convergence != 0) {
-    warning("Optimization did not converge. Code: ", optim_result$convergence)
+    warning("Optimisation did not converge. Code: ", optim_result$convergence)
   }
 
   return(optim_result)
 }
 
 
-plot_marie_fit <- function(surv, fit) {
+plot_model_3_fit <- function(surv, fit) {
+  # Extract observed data
   times <- surv$times
   init <- surv$init
   fin <- surv$fin
   
-  # Extract the fitted parameters
+  # Observed relative change
+  observed_ratio <- fin / init
+  
+  # Extract fitted parameters
+  theta1_hat <- fit$par[1]
+  theta2_hat <- fit$par[2]
+  s2_hat     <- fit$par[3]
+  
+  # Model-predicted relative change
+  # (the expectation of fin/init without the delta_i noise term)
+  predicted_ratio <- theta1_hat * exp(-theta2_hat * times)
+  
+  # Compute the 95% prediction interval in relative space
+  # delta_i ~ N(0, s2_hat) => st. dev of the relative scale is sqrt(s2_hat)
+  ci_multiplier <- 1.96
+  sd_ratio <- sqrt(s2_hat)
+  lower_bound <- predicted_ratio - ci_multiplier * sd_ratio
+  upper_bound <- predicted_ratio + ci_multiplier * sd_ratio
+  
+  # Plot
+  plot(times, observed_ratio,
+       pch = 19, col = "blue",
+       xlab = "Time (hours)",
+       ylab = "Relative Change (fin / init)",
+       main = "Model 3's Fit: Relative Change vs Time")
+  
+  # Sort data by time to ensure a smooth line/polygon
+  ord <- order(times)
+  
+  # Add fitted curve
+  lines(times[ord], predicted_ratio[ord], col = "red", lwd = 2)
+  
+  # Add 95% prediction interval band
+  polygon(
+    x = c(times[ord], rev(times[ord])),
+    y = c(lower_bound[ord], rev(upper_bound[ord])),
+    col = rgb(1, 0, 0, 0.2), border = NA
+  )
+  
+  # Legend
+  legend("topright",
+         legend = c("Observed Ratio", "Fitted Ratio", "95% Prediction Interval"),
+         col = c("blue", "red", rgb(1, 0, 0, 0.2)),
+         pch = c(19, NA, 15), lty = c(NA, 1, NA), bty = "n")
+}
+
+
+# Function to analyse residuals for Model 3
+analyse_model_3_residuals <- function(surv, fit) {
+  # Extract data and fitted parameters
+  times <- surv$times
+  init <- surv$init
+  fin <- surv$fin
+  
   theta1_hat <- fit$par[1]
   theta2_hat <- fit$par[2]
   s2_hat <- fit$par[3]
@@ -100,34 +154,60 @@ plot_marie_fit <- function(surv, fit) {
   # Calculate expected final counts
   expected_fin <- init * theta1_hat * exp(-theta2_hat * times)
   
-  # Calculate 95% prediction intervals (error scales with init)
-  ci_multiplier <- 1.96
-  sd_fin <- init * sqrt(s2_hat)
-  lower_bound <- expected_fin - ci_multiplier * sd_fin
-  upper_bound <- expected_fin + ci_multiplier * sd_fin
-
-  # Plot observed final counts against times
-  plot(times, fin, pch = 19, col = "blue",
-       xlab = "Time (hours)", ylab = "Final Count (fin)",
-       main = "Marie's Model Fit with 95% Prediction Intervals")
-
-  # To draw a smooth curve, order the data by times
-  ord <- order(times)
-  lines(times[ord], expected_fin[ord], col = "red", lwd = 2)
-
-  # Add the prediction interval as a shaded region
-  polygon(c(times[ord], rev(times[ord])),
-          c(lower_bound[ord], rev(upper_bound[ord])),
-          col = rgb(1, 0, 0, 0.2), border = NA)
-
-  legend("topright", legend = c("Observed", "Fitted", "95% Prediction Interval"),
-         col = c("blue", "red", rgb(1, 0, 0, 0.2)),
-         pch = c(19, NA, 15), lty = c(NA, 1, NA), bty = "n")
+  # Calculate raw residuals
+  raw_residuals <- fin - expected_fin
+  
+  # Calculate standardised residuals - scaled by init to match error structure
+  standardised_residuals <- raw_residuals / (init * sqrt(s2_hat))
+  
+  # Create a panel of diagnostic plots
+  par(mfrow = c(2, 2))
+  
+  # Plot 1: Residuals vs Fitted
+  plot(expected_fin, standardised_residuals, 
+       main = "Residuals vs Fitted",
+       xlab = "Fitted Values", 
+       ylab = "Standardised Residuals")
+  abline(h = 0, col = "red", lty = 2)
+  
+  # Plot 2: Residuals vs Time
+  plot(times, standardised_residuals, 
+       main = "Residuals vs Time",
+       xlab = "Time (hours)", 
+       ylab = "Standardised Residuals")
+  abline(h = 0, col = "red", lty = 2)
+  
+  # Plot 3: Normal Q-Q Plot
+  qqnorm(standardised_residuals)
+  qqline(standardised_residuals, col = "red")
+  
+  # Plot 4: Histogram of residuals with normal density overlay
+  hist(standardised_residuals, 
+       main = "Histogram of Residuals",
+       xlab = "standardised Residuals", 
+       probability = TRUE,
+       breaks = 15)
+  curve(dnorm(x, mean = mean(standardised_residuals), sd = sd(standardised_residuals)), 
+        add = TRUE, col = "red", lwd = 2)
+  
+  # Reset the plotting parameters
+  par(mfrow = c(1, 1))
+  
+  
+  # Return residuals and test result for reporting
+  return(list(
+    residuals = raw_residuals,
+    standardised_residuals = standardised_residuals,
+    mean_residual = mean(standardised_residuals),
+    sd_residual = sd(standardised_residuals)
+  ))
 }
 
-
 # Fit the model
-marie_fit <- fit_marie_model(surv)
+model_3_fit <- fit_model_3_model(surv)
+
+# analyse residuals
+residual_analysis <- analyse_model_3_residuals(surv, model_3_fit)
 
 # Ensure the save directory exists
 if (!dir.exists("../plots")) {
@@ -135,8 +215,18 @@ if (!dir.exists("../plots")) {
 }
 
 # Create and save plot of the model fit with 95% prediction intervals
-png(filename = "../plots/marie_fit.png", width = 800, height = 600)
-plot_marie_fit(surv, marie_fit)
+png(filename = "../plots/model_3_fit.png", width = 800, height = 600)
+plot_model_3_fit(surv, model_3_fit)
 dev.off()
+
+# Save the plot for residuals analysis
+png(filename = "../plots/model_3_residuals.png", width = 800, height = 800)
+analyse_model_3_residuals(surv, model_3_fit)
+dev.off()
+
+# Print summary statistics for report
+cat("Residual analysis summary:\n")
+cat("Mean of standardised residuals:", residual_analysis$mean_residual, "\n")
+cat("SD of standardised residuals:", residual_analysis$sd_residual, "\n")
 
 
